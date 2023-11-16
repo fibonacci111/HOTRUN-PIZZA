9ed61a5c4c51d0f0fa1b501f44c6fc46a0b68320
Поповичев Артём Алексеевич <67004012+ppppppiP@users.noreply.github.com>
2023-11-16 22:57:23 +0300

    sdfsdf

--- Assets/Chips/Scripts/Generator_On.cs
+++ Assets/Chips/Scripts/Generator_On.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Generator_On : MonoBehaviour
+{[SerializeField] GameObject battaryes;
+    [SerializeField] GameObject image;
+[SerializeField] Animator anim;
+[SerializeField] GameObject Effects;
+[SerializeField] AudioSource audio;
+[SerializeField] AudioClip Generator;
+[SerializeField] AudioClip EffectsClip;
+private void OnTriggerStay(Collider other)
 {
-    // Start is called before the first frame update
-    void Start()
+    if (other.CompareTag("Player"))
     {
-        
+        image.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
+    if (other.CompareTag("Player"))
     {
-        
+        battaryes.SetActive(false);
+       // anim.CrossFade("Inter", 0.1f);
+       // Effects.SetActive(true);
+        //audio.clip = EffectsClip;
+       // audio.Play();
     }
 }
+private void OnTriggerExit(Collider other)
+{
+    if (other.CompareTag("Player"))
+    {
+        image.SetActive(false);
+    }
+
+}
+
+}
+
